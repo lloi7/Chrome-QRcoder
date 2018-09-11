879d0f2d9d33cd494eada3a1ba69da6f71bfd823
lloi7 <41963211+lloi7@users.noreply.github.com>
2018-09-11 12:55:32 +0800

    increase stability

--- click.js
+++ click.js
@@ -1,3 +1,3 @@
 chrome.tabs.getSelected(null, function (tab) {
-	document.getElementById("QRcodeImage").src="http://qr.liantu.com/api.php?&w=200&text="+tab.url
+	document.getElementById("QRcodeImage").src="http://qr.liantu.com/api.php?&w=200&text=" + encodeURIComponent(tab.url)
 });
